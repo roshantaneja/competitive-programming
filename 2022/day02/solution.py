99991968630bbcffd3330bf1ffda1781558d32b4
Yoshi <rytaneja@gmail.com>
2022-12-01 22:15:39 -0800

    made day 2 worse

--- 2022/day02/solution.py
+++ 2022/day02/solution.py
@@ -2,60 +2,57 @@
 import pathlib
 
 def main():
-	total = 0
-	with open(pathlib.Path("input.txt")) as file:
-		for line in file:
-			moves = line.split()
-			opp = moves[0]
-			you = moves[1]
-			if (you == "X"):
-				total += 1
-			if (you == "Y"):
-				total += 2
-			if (you == "Z"):
-				total += 3
-			if (you == "X" and opp == "A" or you == "Y" and opp == "B" or you == "Z" and opp == "C"):
-				total += 3
-			if (opp == "B" and you == "Z" or opp == "A" and you == "Y" or opp == "C" and you == "X"):
-				total += 6
-	print (total)
-
-def main2():
-	total = 0
+	total1 = 0
+	total2 = 0
 	with open(pathlib.Path("input.txt")) as file:
 		for line in file:
 			moves = line.split()
 			opp = moves[0]
 			you = moves[1]
 			if (opp == "A"):
-				if(you == "X"):
-					total += 3
-				if(you == "Y"):
-					total += 1
-					total += 3
-				if(you == "Z"):
-					total += 2
-					total += 6
+				if (you == "X"):
+					total1 += 3
+					total2 += 3
+					total1 += 1
+				if (you == "Y"):
+					total2 += 1
+					total1 += 6
+					total1 += 2
+					total2 += 3
+				if (you == "Z"):
+					total2 += 2
+					total1 += 3
+					total2 += 6
 			if (opp == "B"):
-				if(you == "X"):
-					total += 1
-				if(you == "Y"):
-					total += 2
-					total += 3
-				if(you == "Z"):
-					total += 3
-					total += 6
+				if (you == "X"):
+					total2 += 1
+					total1 += 1
+				if (you == "Y"):
+					total1 += 3
+					total2 += 2
+					total1 += 2
+					total2 += 3
+				if (you == "Z"):
+					total2 += 3
+					total1 += 6
+					total1 += 3
+					total2 += 6
 			if (opp == "C"):
-				if(you == "X"):
-					total += 2
-				if(you == "Y"):
-					total += 3
-					total += 3
-				if(you == "Z"):
-					total += 1
-					total += 6
-	print (total)
+				if (you == "X"):
+					total2 += 2
+					total1 += 6
+					total1 += 1
+				if (you == "Y"):
+					total2 += 3
+					total1 += 2
+					total2 += 3
+				if (you == "Z"):
+					total1 += 3
+					total2 += 1
+					total1 += 3
+					total2 += 6
+		print(total1)
+		print(total2)
 
 if __name__ == "__main__":
 	main()
-	main2()
