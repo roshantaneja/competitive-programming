223f4b2882a4df5f8e564df6a1def9450257e997
Yoshi <rytaneja@gmail.com>
2022-12-05 21:17:01 -0800

    day 6 soln cleaned up

--- 2022/day06/solution.py
+++ 2022/day06/solution.py
@@ -4,7 +4,12 @@ import heapq
 def main():
 	with open(pathlib.Path("input.txt")) as file:
 		for line in file:
-			heap = []
+			for i in range(len(line)-3):
+				if (len(set(line[i:i+4])) == len(line[i:i+4])):
+					print (i + 4)
+					break
+
+
 			for i in range(len(line)-13):
 				if (len(set(line[i:i+14])) == len(line[i:i+14])):
 					print (i + 14)
